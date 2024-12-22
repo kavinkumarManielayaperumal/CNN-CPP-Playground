4baf6206adc70e64c695a342af30a906005c9d4d
kavin kumar manielayaperumal <kam3694@thi.de>
2024-12-22 21:52:40 +0100

    initial commit

---
+++ trash.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Welcome to your CNN project! okay good now its woking nor jdjiodfokjojkv" << endl;
+    // Your CNN implementation will go here.cd
+    return 0;
+}
